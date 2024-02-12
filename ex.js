c6a91fce7857bf7506ff9a850d5b76943c39a1ee
eualvezx <brunoalvesgame2@gmail.com>
2024-02-12 12:24:05 -0300

    12/02

---
+++ ex.js
@@ -0,0 +1,11 @@
+//console.log('Funcionando liso liso')
+
+var vel = 90
+
+if (vel >= 90){
+    console.log(`Diminua a velocidade`)
+}else
+{
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+}
+
